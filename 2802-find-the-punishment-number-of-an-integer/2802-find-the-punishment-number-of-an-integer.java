f5e3679abd1dd79e2cd76e10934519647a1626c9
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2025-02-15 06:50:05 +0530

    Time: 393 ms (5.64%), Space: 44.4 MB (61.44%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.java
@@ -2,23 +2,30 @@ class Solution {
     public int punishmentNumber(int n) {
         int sum = 0;
         for(int i=1;i<=n;i++){
-            boolean ans = canBePartition(i, String.valueOf(i * i), 0, 0);
+            HashMap<String, Boolean> map = new HashMap<>();
+            boolean ans = canBePartition(i, String.valueOf(i * i), 0, 0, map);
             if(ans){
                 sum += i * i;
             }
         }
         return sum;
     }
-    public boolean canBePartition(int n, String sq, int idx, int sum){
+    public boolean canBePartition(int n, String sq, int idx, int sum, HashMap<String, Boolean> map){
         if(idx == sq.length()){
             return n == sum;
         }
+        String key = idx + " " + sum;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
         for(int i = idx + 1; i <= sq.length(); i++){
             int a = Integer.parseInt(sq.substring(idx, i));
-            if(canBePartition(n, sq, i, sum + a)){
+            if(canBePartition(n, sq, i, sum + a, map)){
+                map.put(key, true);
                 return true;
             }
         }
+        map.put(key, false);
         return false;
     }
 }
