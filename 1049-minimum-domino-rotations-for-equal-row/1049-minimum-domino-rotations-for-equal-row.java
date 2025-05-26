0e4cab244d83ccd0fa76135dc9873b3151c7620e
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2025-05-26 15:38:20 +0530

    Time: 2 ms (100%), Space: 50.7 MB (31.45%) - LeetHub

--- 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
+++ 1049-minimum-domino-rotations-for-equal-row/1049-minimum-domino-rotations-for-equal-row.java
@@ -1,47 +1,29 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int ele: tops){
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
-        }
-        for(int ele: bottoms){
-            map.put(ele, map.getOrDefault(ele, 0) + 1);
-        }
+        int ans = Integer.MAX_VALUE;
 
-        int ans = -1;
-
-        for(int key: map.keySet()){
-            if(map.get(key) >= tops.length){
-                int count = 0, count1 = 0;
-                boolean f = false;
-                for(int i=0;i<tops.length;i++){
-                    if(tops[i] != key){
-                        if(bottoms[i] == key){
-                            count++;
-                        }
-                        else{
-                            f = true;
-                            break;
-                        }
-                    }
-                }
-                for(int i=0;i<bottoms.length;i++){
-                    if(bottoms[i] != key){
-                        if(tops[i] == key){
-                            count1++;
-                        }
-                        else{
-                            f = true;
-                            break;
-                        }
-                    }
-                }
-                
-                if(!f){
-                    ans = Math.min(count, count1);
-                }
+        for(int i=1;i<=6;i++){
+            int swap = findSwap(tops, bottoms, i);
+            if(swap != -1){
+                ans = Math.min(ans, swap);
             }
         }
-        return ans;
+
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+    public int findSwap(int[] tops, int[] bottoms, int n){
+        int top = 0, bottom = 0;
+        for(int i=0;i<tops.length;i++){
+            if(tops[i] != n && bottoms[i] != n){
+                return -1;
+            }
+            if(tops[i] != n){
+                top++;
+            }
+            if(bottoms[i] != n){
+                bottom++;
+            }
+        }
+        return Math.min(top, bottom);
     }
 }
