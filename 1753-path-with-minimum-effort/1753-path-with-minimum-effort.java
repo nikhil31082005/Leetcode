848e2ac0989c2e2d26c424dd63cb838e60c3b823
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2025-09-07 22:37:21 +0530

    Time: 39 ms (76.6%), Space: 45 MB (90.03%) - LeetHub

--- 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/1753-path-with-minimum-effort.java
@@ -1,45 +1,41 @@
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        pq.add(new int[]{0, 0, 0});
-        int rows = heights.length;
-        int cols = heights[0].length;
-        int[] dx = {0,0,1,-1};
-        int[] dy = {1,-1,0,0};
-        boolean[][] visited = new boolean[rows][cols];
-        // visited[0][0] = true;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[2] - b[2];
+        });
 
-        int[][] effort = new int[rows][cols];
-        for(int[] arr: effort){
-            Arrays.fill(arr,Integer.MAX_VALUE);
+        int[][] efforts = new int[heights.length][heights[0].length];
+        for(int[] arr: efforts) {
+            Arrays.fill(arr, Integer.MAX_VALUE);
         }
-        effort[0][0] = 0;
 
-        while(!pq.isEmpty()){
+        efforts[0][0] = 0;
+
+        pq.add(new int[]{0, 0, 0});
+
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+
+        while(!pq.isEmpty()) {
             int[] curr = pq.poll();
-            int r = curr[0];
-            int c = curr[1];
+            int row = curr[0];
+            int col = curr[1];
             int currEffort = curr[2];
 
-            if(r == rows-1 && c == cols-1){
+            if(row == heights.length - 1 && col == heights[0].length - 1) {
                 return currEffort;
             }
 
-            if(visited[r][c]){
-                continue;
-            }
+            for(int i = 0; i < 4; i++) {
+                int nr = row + dx[i];
+                int nc = col + dy[i];
 
-            visited[r][c] = true;
+                if(nr >= 0 && nr < heights.length && nc >= 0 && nc < heights[0].length) {
+                    int newEffort = Math.max(currEffort, Math.abs(heights[row][col] - heights[nr][nc]));
 
-            for(int i=0;i<4;i++){
-                int nx = dx[i] + r;
-                int ny = dy[i] + c;
-                if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && !visited[nx][ny]){
-                    int newEffort = Math.max(currEffort, Math.abs(heights[nx][ny] - heights[r][c]));
-
-                    if (newEffort < effort[nx][ny]) {
-                        effort[nx][ny] = newEffort;
-                        pq.add(new int[]{nx, ny, newEffort});
+                    if(newEffort < efforts[nr][nc]) {
+                        efforts[nr][nc] = newEffort;
+                        pq.add(new int[]{nr, nc, newEffort});
                     }
                 }
             }
