f34196a4709561871a6afd35bcf8715ce7986b9e
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2025-09-27 19:42:47 +0530

    Time: 15 ms (53.24%), Space: 46.7 MB (64.23%) - LeetHub

--- 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/1388-greatest-sum-divisible-by-three.java
@@ -1,33 +1,20 @@
 class Solution {
     public int maxSumDivThree(int[] nums) {
-        int sum = 0;
-        int n = nums.length;
-
-        List<Integer> rem1 = new ArrayList<>();
-        List<Integer> rem2 = new ArrayList<>();
+        int[] dp = new int[3];
+        Arrays.fill(dp, Integer.MIN_VALUE);
+        dp[0] = 0;
 
         for(int ele: nums) {
-            sum += ele;
-            if(ele % 3 == 1) rem1.add(ele);
-            if(ele % 3 == 2) rem2.add(ele); 
+            int[] curr = dp.clone();
+            for(int i=0;i<3;i++){
+                if(dp[i] != Integer.MIN_VALUE) {
+                    int sum = dp[i] + ele;
+                    int newR = sum % 3;
+                    curr[newR] = Math.max(curr[newR], sum);
+                }
+            }
+            dp = curr;
         }
-
-        Collections.sort(rem1);
-        Collections.sort(rem2);
-
-        if(sum % 3 == 0) {
-            return sum;
-        }
-        if(sum % 3 == 1) {
-            int option1 = rem1.size() >= 1 ? sum - rem1.get(0) : 0;
-            int option2 = rem2.size() >= 2 ? sum - rem2.get(0) - rem2.get(1) : 0;
-            return Math.max(option1, option2);
-        }
-        if(sum % 3 == 2) {
-            int option1 = rem2.size() >= 1 ? sum - rem2.get(0) : 0;
-            int option2 = rem1.size() >= 2 ? sum - rem1.get(0) - rem1.get(1) : 0;
-            return Math.max(option1, option2);
-        }
-        return 0;
+        return dp[0];
     }
 }
