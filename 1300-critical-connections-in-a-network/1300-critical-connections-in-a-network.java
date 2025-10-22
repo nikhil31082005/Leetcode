b139bad0e0546eb41fd766efa2c3802593ffbb18
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2025-10-22 07:21:46 +0530

    Time: 95 ms (41.98%), Space: 115.7 MB (71.69%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,44 +1,44 @@
 class Solution {
-    private int timer = 1;
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         List<List<Integer>> graph = new ArrayList<>();
         for(int i=0;i<n;i++){
             graph.add(new ArrayList<>());
         }
-        for(List<Integer> ls: connections){
+
+        for(List<Integer> ls: connections) {
             graph.get(ls.get(0)).add(ls.get(1));
             graph.get(ls.get(1)).add(ls.get(0));
         }
 
-        List<List<Integer>> bridges = new ArrayList<>();
-
-        boolean[] visited = new boolean[n];
-        int[] tin = new int[n];
+        int[] time = new int[n];
         int[] low = new int[n];
+        boolean[] visited = new boolean[n];
 
-        dfs(0, -1, visited, tin, low, graph, bridges);
-        return bridges;
+        dfs(graph, 0, 1, time, low, visited, -1);
+        return ans;
     }
-    public void dfs(int node, int parent, boolean[] visited, int[] tin, int[] low, List<List<Integer>> graph, List<List<Integer>> bridges) {
-        visited[node] = true;
-        tin[node] = timer;
-        low[node] = timer;
-        timer++;
-        for(int nbr: graph.get(node)){
+    public void dfs(List<List<Integer>> graph, int src, int t, int[] time, int[] low, boolean[] visited, int parent) {
+        time[src] = t;
+        low[src] = t;
+        visited[src] = true;
+        
+        for(int nbr: graph.get(src)) {
             if(nbr == parent) continue;
-
             if(!visited[nbr]) {
-                dfs(nbr, node, visited, tin, low, graph, bridges);
+                dfs(graph, nbr, t + 1, time, low, visited, src);
 
-                low[node] = Math.min(low[nbr], low[node]);
+                low[src] = Math.min(low[src], low[nbr]);
 
-                if(tin[node] < low[nbr]){
-                    bridges.add(Arrays.asList(node, nbr));
+                if(time[src] < low[nbr]) {
+                    ans.add(Arrays.asList(src, nbr));
                 }
             }
             else {
-                low[node] = Math.min(low[node], tin[nbr]);
+                low[src] = Math.min(low[src], low[nbr]);
             }
+
+
         }
     }
 }
