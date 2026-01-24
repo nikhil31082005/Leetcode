1fc3a6342d9d7a294ffdd4adff9e1ec501698bc6
Nikhil <122168803+nikhil31082005@users.noreply.github.com>
2026-01-24 19:17:24 +0530

    Time: 2 ms (99.46%), Space: 43.6 MB (38.8%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,23 +1,28 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] primes = {2, 3, 5};
-        PriorityQueue<Long> uglyHeap = new PriorityQueue<>();
-        HashSet<Long> visited = new HashSet<>();
-        
-        uglyHeap.add(1L);
-        visited.add(1L);
-        
-        long curr = 1L;
-        for (int i = 0; i < n; i++) {
-            curr = uglyHeap.poll();
-            for (int prime : primes) {
-                long new_ugly = curr * prime;
-                if (!visited.contains(new_ugly)) {
-                    uglyHeap.add(new_ugly);
-                    visited.add(new_ugly);
-                }
+        int[] ugly = new int[n];
+        ugly[0] = 1;
+
+        int i2 = 0, i3 = 0, i5 = 0;
+
+        for(int i=1;i<n;i++){
+            int next2 = ugly[i2] * 2;
+            int next3 = ugly[i3] * 3;
+            int next5 = ugly[i5] * 5;
+
+            int nextugly = Math.min(next2, Math.min(next3, next5));
+            ugly[i] = nextugly;
+
+            if(nextugly == next2) {
+                i2++;
+            }
+            if(nextugly == next3) {
+                i3++;
+            }
+            if(nextugly == next5) {
+                i5++;
             }
         }
-        return (int)curr;
+        return ugly[n-1];
     }
 }
